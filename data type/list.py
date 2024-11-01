f2396e61f12bdfc25c6219d579d8b30ac09fc11b
ShuvoDAI <mostofa@devolvedai.com>
2024-11-02 00:33:58 +0600

    string and list done

---
+++ data type/list.py
@@ -0,0 +1,36 @@
+# l = []
+# l.append(1)
+# l.append(2)
+# l.append(3)
+
+# l.pop(0)
+# print(l)
+
+# l.pop(0)
+# print(l)
+
+
+# stack
+
+# l = []
+# l.append(1)
+# l.append(2)
+# l.append(3)
+
+# l.pop()
+# l.pop()
+
+# print(l)
+
+
+# l = [1, 2, 3]
+# #[int_index: end_index+1: step]
+# a = l[1:3:1]
+
+# print(a)
+
+
+l = [1, 4, 3]
+l.sort(reverse=True)
+
+print(l)
---
+++ data type/string.py
@@ -0,0 +1,12 @@
+# a = "We are DUETian"
+# b = a.replace('e', 'i', 2)
+
+# print(b)
+
+
+a = "We are DUETian"
+b = "DUET"
+# a[in_index:end_index+1: step]
+s = a + " " + b
+
+print(s)
